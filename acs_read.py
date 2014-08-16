11965df48f3552f0051f95021f9eb09b53cdc3a5
bjb40 <bryce.bartlett@duke.edu>
2014-08-16 06:48:09 -0400

    first commit

---
+++ acs_read.py
@@ -0,0 +1,11 @@
+
+import pandas
+
+datref = "../data/acs 3 year sets/g20123ca.csv"
+
+pdat = pandas.read_csv(datref)
+
+for i in range(0,50):
+   print i
+  
+
---
+++ pedaccidents.py
@@ -0,0 +1,33 @@
+# <! Python 2.7
+# Bryce
+
+# dependancies: pyshp 1.2.1: https://pypi.python.org/pypi/pyshp
+
+import os
+os.chdir("H:/Academic Projects/datathon/scripts")
+
+import shapefile
+
+dbf = open("H:/Academic Projects/datathon/data/pedestrian victims/PedVictimsKSI.dbf", "rb")
+shp = open("H:/Academic Projects/datathon/data/pedestrian victims/PedVictimsKSI.shp", "rb")
+
+shapefile = shapefile.Reader(shp=shp, dbf=dbf)
+
+
+records = shapefile.records()
+fields = shapefile.fields
+
+#available fields
+print fields
+
+#total records
+print("\nTotal incidents: "+ str(len(records))+"\n")
+
+
+
+for i in range(0,len(records)):
+    print records[i][15] #neighborhood
+    print records[i][4] #year
+
+
+
